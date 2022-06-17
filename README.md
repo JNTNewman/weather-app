26b6e2855735be9633a61183314185e20a434db1
John Newman <jnt.newman@outlook.com>
2022-06-17 16:28:25 -0500

    Refactor document queries for 'DRYness'.

---
+++ README.md
@@ -0,0 +1,41 @@
+# Javascript Weather App
+
+## Welcome 👋
+
+Welcome to this Javascript weather app repository.
+
+## The Inspiration
+
+I came across a very well-crafted weather app built in Vue [Vue JS Weather App](https://dogukanbatal.github.io/vue-weather-app) by Dogukan Batal and thought of doing the same to hone my HTML, CSS and Javascript skills.
+
+This weather app utilizes the [OpenWeather API](https://openweathermap.org/) and you will need to set up an account (it has a free tier) and get an API key to use with the app.
+
+---
+
+### Favicon
+
+I used the sun icon from FreeIconsPNG site.
+<a href="https://www.freeiconspng.com/img/8579" title="Image from freeiconspng.com"><img src="https://www.freeiconspng.com/uploads/sun-icon-22.png" width="32" alt="Download Sun Icon" /></a>
+
+### Project Setup
+
+- Sign up and create and API Key at [OpenWeatherMap](https://openweathermap.org/).
+- Store your key in a javascript file such as store.js or env.js
+
+`const apiKey = "YOUR_API_KEY";`
+`export default apiKey;`
+
+This is used in the main.js file following import:
+`import apiKey from './store.js';`
+
+## Description
+
+The app takes city name query and returns information about weather conditions: temperature, feel, wind speed, sky condition, humidity, cloud cover, sunrise, sunset and actual time in that city. The background of the container div changes depending on the weather conditions and night and day. Images for the background are from [Unsplash.com](https://unsplash.com/). I have tried to have images to cover all possible conditions but I may have missed some.
+
+You may also input the query string as City Name, Country (eg. Miami, US)
+
+## Use
+
+Feel free to clone, fork and improve as you desire.
+
+*John Newman*
--- main.js
+++ main.js
@@ -1,80 +1,149 @@
-import appid from './store.js';
+import apiKey from './store.js'
 
-let baseApiUrl = 'https://api.openweathermap.org/data/2.5/weather?';
+let baseApiUrl = 'https://api.openweathermap.org/data/2.5/weather?'
 
-let cityName = 'Mandeville'; // default city
+if (window.navigator.geolocation) {
+    window.navigator.geolocation.getCurrentPosition((position) => {
+                            console.log(position.coords.latitude)
+            });
+} 
 
-window.addEventListener("load", (e) => {
-    e.preventDefault();
-    document.querySelector("input").value = cityName;
+
+
+let cityName = 'Kathmandu' // default city
+let input = document.querySelector('input')
+let button = document.querySelector('button')
+let cityInput = document.querySelector('#city-input')
+let backgroundContainer = document.querySelector('.bg-div')
+let sunriseTag = document.querySelector('#sunrise')
+let sunsetTag = document.querySelector('#sunset')
+let weatherLocationTimeTag = document.querySelector('#weather-location-time')
+let weatherConditionTag = document.querySelector('#condition')
+let humidityTag = document.querySelector('#humidity')
+let temperatureTag = document.querySelector('#temperature')
+let countryTag = document.querySelector('.country')
+let weatherIconTag = document.querySelector('#weather-icon')
+let cloudCoverTag = document.querySelector('#cloud-cover')
+let windSpeedTag = document.querySelector('#wind')
+let actualFeelTag = document.querySelector('#feels-like')
+
+window.addEventListener('load', e => {
+  e.preventDefault()
+  input.value = cityName
+  getWeatherData(cityName)
+})
+
+input.addEventListener('keypress', e => {
+  if (e.key === 'Enter') {
+    e.preventDefault()
+    cityName = input.value
     getWeatherData(cityName)
-});
+  }
+})
 
-document.querySelector("input").addEventListener("keypress", (e) => {
-    if(e.key === "Enter") {
-        e.preventDefault();
-    cityName = document.querySelector("input").value;
-    getWeatherData(cityName);  
-    }
-});
+button.addEventListener('click', e => {
+  e.preventDefault()
+  cityName = input.value
+  getWeatherData(cityName)
+})
 
-document.querySelector("button").addEventListener("click", (e) => {
-    e.preventDefault();
-    cityName = document.querySelector("input").value;
-    getWeatherData(cityName);
-});
+cityInput.addEventListener('focusin', e => {
+  cityInput.value = ' '
+  cityInput.placeholder = `${cityName}`
+})
 
-function capitalizeWords(sentenceString) {
-    let __tmpArray = [];
-    const wordsArray = sentenceString.split(' ');
-    wordsArray.forEach(word => {
-        const capitalizedWord = word.slice(0, 1).toLocaleUpperCase() + word.slice(1)
-       __tmpArray.push(capitalizedWord);
-    });
-    return __tmpArray.join(' ');
+cityInput.addEventListener('focusout', e => {
+  cityInput.value = `${cityName}`
+  cityInput.placeholder = `${cityName}`
+})
+
+function capitalizeWords (sentenceString) {
+  let __tmpArray = []
+  const wordsArray = sentenceString.split(' ')
+  wordsArray.forEach(word => {
+    const capitalizedWord = word.slice(0, 1).toLocaleUpperCase() + word.slice(1)
+    __tmpArray.push(capitalizedWord)
+  })
+  return __tmpArray.join(' ')
 }
 
-async function getWeatherData(city) {
-    const url = baseApiUrl + 'q=' + city + '&units=metric&appid=' + appid;
-    let response = await fetch(url);
-    const responseStatus = response.status;
-    if (responseStatus === 200){
-        const data = await response.json();
-            const weather = data.weather[0];
-            let weatherState = weather.description;
-            const conditionString = weather.main.toLowerCase();
-            weatherState = capitalizeWords(weatherState);
-            const localeTZ = new Date().getTimezoneOffset()/60;
-            const weatherTZ = data.timezone/3600;
-            const sunrise = new Date((data.sys.sunrise * 1000) + ((localeTZ + weatherTZ) * 3600000));
-            const sunset = new Date((data.sys.sunset * 1000) + ((localeTZ + weatherTZ) * 3600000));
+async function getWeatherData (city) {
+  const url = baseApiUrl + 'q=' + city + '&units=metric&appid=' + apiKey
+  let response = await fetch(url)
+  const responseStatus = response.status
+  if (responseStatus === 200) {
+    const data = await response.json()
+    const weather = data.weather[0]
+    let weatherState = weather.description
+    const conditionString = weather.main.toLowerCase()
+    weatherState = capitalizeWords(weatherState)
+    const localeTZ = new Date().getTimezoneOffset() / 60
+    const weatherTZ = data.timezone / 3600
+    const weatherLocationDate = new Date(
+      data.dt * 1000 + (localeTZ + weatherTZ) * 3600000
+    )
+    const sunriseDate = new Date(
+      data.sys.sunrise * 1000 + (localeTZ + weatherTZ) * 3600000
+    )
+    const sunsetDate = new Date(
+      data.sys.sunset * 1000 + (localeTZ + weatherTZ) * 3600000
+    )
+    const sunriseTime = sunriseDate.toLocaleString('en-US', {
+      hour: 'numeric',
+      minute: 'numeric',
+      hour12: true
+    })
+    const sunsetTime = sunsetDate.toLocaleString('en-US', {
+      hour: 'numeric',
+      minute: 'numeric',
+      hour12: true
+    })
+    const weatherLocationTime = weatherLocationDate.toLocaleString('en-US', {
+      hour: 'numeric',
+      minute: 'numeric',
+      hour12: true
+    })
 
-            document.querySelector(".bg-div").innerHTML = `<img src=./images/skies/${conditionString}.jpg alt=${conditionString} />`;
-            document.querySelector("#condition").innerText = ` ${weatherState}`; 
-            document.querySelector(".country").innerHTML = `<span>(${data.sys.country})</span>`;
-            document.querySelector("#weather-icon").innerHTML = `<img class="weather-icon" src=http://openweathermap.org/img/wn/${weather.icon}@2x.png alt=${condition.icon}/>`
-            document.querySelector("#temperature").innerHTML = `<span class="temperature">${Math.round(data.main.temp)}<sup>°</sup></span>`;
-            
-            document.querySelector("#feels-like").innerHTML = `Feels Like <strong>${Math.round(data.main.feels_like)}°C</strong>`;           
-            document.querySelector("#humidity").innerHTML = ` ${data.main.humidity}%`;
-            document.querySelector("#cloud-cover").innerText = ` ${data.clouds.all}%`;
-            document.querySelector("#wind").innerText = ` ${data.wind.speed.toFixed(1)} m/s`;
-            document.querySelector("#sunrise").innerText = `${sunrise.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })}`;
-            document.querySelector("#sunset").innerText = `${sunset.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })}`;
-        }
-        else {
-        document.querySelector("#condition").innerText = `Data for ${city} was ${response.statusText}`;
-        document.querySelector("#condition").style.color = "red";
-        document.querySelector("#condition").style.fontSize = "1.0rem";
-        document.querySelector(".bg-div").innerHTML = `<img src=./images/skies/gray.jpg alt="gray background" />`;
-        document.querySelector(".country").innerHTML = `<span>(${city})</span>`;
-        document.querySelector("#temperature").innerHTML = `<span class="temperature">0<sup>°</sup></span>`;
-        document.querySelector("#feels-like").innerHTML = " ";
-        document.querySelector("#humidity").innerHTML = "0%";
-        document.querySelector("#cloud-cover").innerText = "0%";
-        document.querySelector("#wind").innerText = "0 m/s";
-        document.querySelector("#sunrise").innerText = " ";
-        document.querySelector("#sunset").innerText = " ";
-        }
-};
-    
+    if (
+      weatherLocationDate.getHours() > sunriseDate.getHours() &&
+      weatherLocationDate.getHours() < sunsetDate.getHours()
+    ) {
+      backgroundContainer.innerHTML = `<img src=./images/skies/${conditionString}.jpg alt=${conditionString} />`
+    } else {
+      backgroundContainer.innerHTML = `<img src=./images/skies/${conditionString}-night.jpg alt=${conditionString} />`
+    }
+
+    cityInput.placeholder = `${city}`
+    countryTag.innerHTML = `<span>(${data.sys.country})</span>`
+    weatherConditionTag.style.removeProperty('color')
+    weatherConditionTag.innerText = ` ${weatherState}`
+    weatherIconTag.innerHTML = `<img class="weather-icon" src=http://openweathermap.org/img/wn/${weather.icon}@2x.png alt=${condition.icon}/>`
+    temperatureTag.innerHTML = `<span class="temperature">${Math.round(
+      data.main.temp
+    )}<sup>°</sup></span>`
+
+    actualFeelTag.innerHTML = `Feels Like <strong>${Math.round(
+      data.main.feels_like
+    )}°C</strong>`
+    humidityTag.innerHTML = ` ${data.main.humidity}%`
+    cloudCoverTag.innerText = ` ${data.clouds.all}%`
+    windSpeedTag.innerText = ` ${data.wind.speed.toFixed(1)} m/s`
+    sunriseTag.innerText = sunriseTime
+    weatherLocationTimeTag.innerText = weatherLocationTime
+    sunsetTag.innerText = sunsetTime
+  } else {
+    backgroundContainer.innerHTML = `<img src=./images/skies/404.jpg alt="gray background" />`
+    cityInput.placeholder = `${city}`
+    countryTag.innerHTML = `<span>(${city})</span>`
+    weatherConditionTag.innerText = `${city} ${response.statusText}`
+    weatherConditionTag.style.setProperty('color', 'red')
+    weatherConditionTag.style.fontSize = '1.0rem'
+    temperatureTag.innerHTML = `<span class="temperature">-<sup>°</sup></span>`
+    actualFeelTag.innerHTML = ' '
+    humidityTag.innerHTML = ' - %'
+    cloudCoverTag.innerText = ' - %'
+    windSpeedTag.innerText = ' - m/s'
+    sunriseTag.innerText = ' '
+    sunsetTag.innerText = ' '
+  }
+}
